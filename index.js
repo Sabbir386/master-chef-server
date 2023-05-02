fd7ee530e432a12fd6fee9caf9681fadac6aea32
Sabbir386 <sabbir386.sa@gmail.com>
2023-05-02 20:29:50 +0600

    added chef master data from server

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const categories = require('./data/categories.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('master cheef is running');
 });
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
 app.listen(port, () => {
     console.log(`master chef api is running on port : ${port}`);
 })
